2e945b236964bacf7b9b93a813058f11f4958b2c
Anthony McMellen <anthonymc456@gmail.com>
2020-10-04 16:23:39 -0400

    Adds index and styling for the page

---
+++ public/main.js
@@ -0,0 +1,28 @@
+let ejs = require('ejs');
+let express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'))
+
+app.get("/", function(req, res){
+  res.render("index", {title: "Hello World", message: "this is the message", img: "hassan-logo.png"})
+})
+
+app.get("/about", function(req, res){
+  res.render("about", {title: "About Me", message: "This is a message that tells people a little bit about myself", img: "hassan-logo.png"})
+})
+
+app.get("/projects", function(req, res){
+  res.render("projects", {title: "My Projects", message: "This is a project that i am showing you", logo: "hassan-logo.png", p1: "http://placekitten.com/200/300", p2: "http://placekitten.com/200/300", p3: "http://placekitten.com/200/300", p1des: "first project", p2des: "second project", p3des: "third project"})
+})
+
+app.get("/contact", function(req, res){
+  res.render("contact", {title: "Contact Me", message: "I can be reached with any of these contacts here", img: "hassan-logo.png"})
+})
+
+
+
+app.listen(port , () => console.log(`Template engine listening on port ${port}`));
